39e59b40404d07439119e7d57c32072e6210b495
jstorrs001 <nsu32kicker@yahoo.com>
2016-08-17 13:35:25 -0500

    New setup files.

---
+++ src/test/java/com/sf/codingcomp/setup/SetupTest.java
@@ -0,0 +1,14 @@
+package com.sf.codingcomp.setup;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SetupTest {
+
+	@Test
+	public void testJUnitDependency() {
+		assertTrue(true);
+	}
+
+}
